1f19cd47847ca7b432ef38d073ed88878e89c58d
Thaweesak Chusri <thaweesak@nimeyo.com>
2020-02-17 21:25:12 +0700

    updated dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
-FROM nginx:1.15-alpine 
-RUN apk add inotify-tools certbot openssl ca-certificates
+FROM nginx:1.17.6-alpine
+
+RUN apk add bash inotify-tools certbot openssl ca-certificates 
 WORKDIR /opt
+
 COPY entrypoint.sh nginx-letsencrypt
 COPY certbot.sh certbot.sh
-COPY default.conf /etc/nginx/conf.d/default.conf
+
+# COPY default.conf /etc/nginx/conf.d/default.conf
 COPY ssl-options/ /etc/ssl-options
 RUN chmod +x nginx-letsencrypt && \
     chmod +x certbot.sh 
+
+VOLUME ["/domain-management"]
+
 ENTRYPOINT ["./nginx-letsencrypt"]
---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+volumne
+REPO=registry.logicspark.com/nginx
+VERSION=1.17.6-letsencrypt
+CONTAINER_NAME=load-balance
+
+docker network create net-nginx
+
+docker rm -f $CONTAINER_NAME
+docker build -t ${REPO}:${VERSION} .
+docker run -d \
+    --name $CONTAINER_NAME \
+    --network net-nginx \
+    -v `pwd`/domain-management:/opt/domain-management \
+    -v `pwd`/certs:/etc/letsencrypt \
+    -v `pwd`/nginx-conf:/etc/nginx/conf.d/ \
+    -e DOMAIN=my.domain.example.com \
+    -e EMAIL=my.email@my.provider.example.com \
+    -p 80:80 \
+    -p 443:443 \
+    ${REPO}:${VERSION}
+
+# docker push ${REPO}:${VERSION}
